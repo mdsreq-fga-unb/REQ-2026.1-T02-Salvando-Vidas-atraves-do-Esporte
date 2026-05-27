e47cfb8e6db3bab5bcdb29e38d5ea09e161ad75a
José Moraes <joseaugustomoraes@protonmail.com>
2026-05-27 20:12:55 -0300

    refactor/Remove código de exemplo

--- src/lib/ui/home/view_models/home_view_models.dart
+++ src/lib/ui/home/view_models/home_view_models.dart
@@ -1,28 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:logger/logger.dart';
-import 'package:provider/provider.dart';
-import 'package:salvando_vidas/ui/home/widgets/body.dart';
-import 'package:salvando_vidas/data/stores/home/counter.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final counter = Counter();
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      ),
-      body: homeCounter(context, counter),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          counter.increment();
-          context.read<Logger>().i("Counter Incremented");
-        },
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+    return Scaffold(body: Center(child: const Text("Dashboard")));
   }
 }
